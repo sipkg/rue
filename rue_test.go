57dad50504153ee52327584553f035b08d662ad4
Brahim Hamdouni <brahim@hamdouni.com>
2021-10-10 00:55:40 +0200

    Add benchmark

--- rue_test.go
+++ rue_test.go
@@ -268,3 +268,154 @@ func TestPostFormParameters(t *testing.T) {
 		t.Errorf(`Param("z") = %q, want "bar"`, z)
 	}
 }
+
+//
+// B E N C H M A R K S :
+//
+
+// data needed for benchmarking
+type benchmarkData struct {
+	RouteMethod  string
+	RoutePattern string
+
+	Method string
+	Path   string
+}
+
+// only needed to prevent the compiler from optimizing the code too much
+var benchmarkCounter int
+
+// dummy routes used to fill up the routes table and make the benchmark more interesting
+var benchmarkDummyRoutes = []string{
+	// don't match at all:
+	"/nomatch/one",
+	"/nomatch/two",
+	"/nomatch/three",
+	"/nomatch/four",
+	"/nomatch/five",
+	"/nomatch/six",
+	"/nomatch/seven",
+	"/nomatch/eight",
+	"/nomatch/nine",
+	"/nomatch/ten",
+
+	// matching one:
+	"/one/nomatch/one",
+	"/one/nomatch/two",
+	"/one/nomatch/three",
+	"/one/nomatch/four",
+	"/one/nomatch/five",
+	"/one/nomatch/six",
+	"/one/nomatch/seven",
+	"/one/nomatch/eight",
+	"/one/nomatch/nine",
+	"/one/nomatch/ten",
+
+	// matching two:
+	"/one/two/nomatch/one",
+	"/one/two/nomatch/two",
+	"/one/two/nomatch/three",
+	"/one/two/nomatch/four",
+	"/one/two/nomatch/five",
+	"/one/two/nomatch/six",
+	"/one/two/nomatch/seven",
+	"/one/two/nomatch/eight",
+	"/one/two/nomatch/nine",
+	"/one/two/nomatch/ten",
+
+	// matching three:
+	"/one/two/three/nomatch/one",
+	"/one/two/three/nomatch/two",
+	"/one/two/three/nomatch/three",
+	"/one/two/three/nomatch/four",
+	"/one/two/three/nomatch/five",
+	"/one/two/three/nomatch/six",
+	"/one/two/three/nomatch/seven",
+	"/one/two/three/nomatch/eight",
+	"/one/two/three/nomatch/nine",
+	"/one/two/three/nomatch/ten",
+}
+
+func BenchmarkStaticTwo(b *testing.B) {
+	benchmarkWay(b, benchmarkData{
+		"GET", "/one/two",
+		"GET", "/one/two",
+	})
+}
+func BenchmarkStaticThree(b *testing.B) {
+	benchmarkWay(b, benchmarkData{
+		"GET", "/one/two/three",
+		"GET", "/one/two/three",
+	})
+}
+func BenchmarkStaticFour(b *testing.B) {
+	benchmarkWay(b, benchmarkData{
+		"GET", "/one/two/three/four",
+		"GET", "/one/two/three/four",
+	})
+}
+func BenchmarkParamsTwo(b *testing.B) {
+	benchmarkWay(b, benchmarkData{
+		"GET", "/:one/:two",
+		"GET", "/one/two",
+	})
+}
+func BenchmarkParamsThree(b *testing.B) {
+	benchmarkWay(b, benchmarkData{
+		"GET", "/:one/:two/:three",
+		"GET", "/one/two/three",
+	})
+}
+func BenchmarkParamsFour(b *testing.B) {
+	benchmarkWay(b, benchmarkData{
+		"GET", "/:one/:two/:three/:four",
+		"GET", "/one/two/three/four",
+	})
+}
+func BenchmarkNoMatchStaticThree(b *testing.B) {
+	benchmarkWay(b, benchmarkData{
+		"GET", "/one/two/three/four",
+		"GET", "/one/two/three",
+	})
+}
+func BenchmarkNoMatchParamsThree(b *testing.B) {
+	benchmarkWay(b, benchmarkData{
+		"GET", "/:one/:two/:three/:four",
+		"GET", "/one/two/three",
+	})
+}
+func BenchmarkNoMatchStaticFour(b *testing.B) {
+	benchmarkWay(b, benchmarkData{
+		"GET", "/one/two/three/four",
+		"GET", "/one/two/three",
+	})
+}
+func BenchmarkNoMatchParamsFour(b *testing.B) {
+	benchmarkWay(b, benchmarkData{
+		"GET", "/:one/:two/:three/:four",
+		"GET", "/one/two/three",
+	})
+}
+
+func benchmarkWay(b *testing.B, d benchmarkData) {
+	r := NewRouter()
+	count := 0
+	for _, pattern := range benchmarkDummyRoutes {
+		r.Handle(d.RouteMethod, pattern, http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			b.Errorf("Routed to wrong method in benchmark!")
+		}))
+	}
+	r.Handle(d.RouteMethod, d.RoutePattern, http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		count++
+	}))
+	req, err := http.NewRequest(d.Method, d.Path, nil)
+	if err != nil {
+		b.Errorf("NewRequest: %s", err)
+	}
+	w := httptest.NewRecorder()
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		r.ServeHTTP(w, req)
+	}
+	benchmarkCounter = count
+}
